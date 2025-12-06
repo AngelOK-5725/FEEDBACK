38c0895c3c677ec43cf61f522e981f8b4eb89f74
Тамила <hamraevatamila@gmail.com>
2025-12-06 17:31:00 +0500

    *

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6n+*-qbe=u^bqt@#75zg+(g+9c^0ldf8a#^!^gc9&&eq-hfed%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
